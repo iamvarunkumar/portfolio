38ac757383ca6a121515313e75199a03232e4801
Varun Kumar <iamvarunkumar23@gmail.com>
2025-04-22 19:57:41 +0530

    Fix: Add https://my-portfolio-764278504344.asia-south2.run.app to ALLOWED_HOST

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = 'django-insecure-n3p1-jt(iys9l!6_5*zd1c_4lkq)*k3z_up@ibd6@mo$&$7c#@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","my-portfolio-857911444995.us-central1.run.app"]
+ALLOWED_HOSTS = ["*","my-portfolio-764278504344.asia-south2.run.app"]
 
 
 # Application definition
